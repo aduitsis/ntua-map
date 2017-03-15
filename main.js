9c841917562db4645c0e17ccf9e88d9532a50410
Athanasios Douitsis <aduitsis@cpan.org>
2017-03-15 17:18:56 +0200

    Make ntua geojson to be loaded dynamically
    - ntua.json is now loaded using xhr.
    - cordinate zooming based on points

--- main.js
+++ main.js
@@ -1,43 +1,20 @@
-define("main",['require','https://unpkg.com/leaflet@1.0.3/dist/leaflet.js'],function(require,leaflet) {
+define('main',
+	[
+	'require',
+	'leaflet/leaflet',
+	'jquery/jquery-3.1.1.min',
+	],
+	function(require,leaflet) {
     // refer to RFC7946 for The GeoJSON Format
     // https://tools.ietf.org/html/rfc7946
-    var f = {
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "coordinates": [
-                        [
-                            23.781981,
-                            37.977722
-                        ],
-                        [
-                            23.782434,
-                            37.977281
-                        ],
-                        [
-                            23.784898,
-                            37.978174
-                        ],
-                        [
-                            23.78305,
-                            37.979028
-                        ]
-                    ],
-                    "type": "MultiPoint"
-                },
-                "id": "77005bc5865fea0cad158d1b31880635"
-            }
-        ],
-        "type": "FeatureCollection"
-    };
+	var init_lat = 37.97765252366751 , init_lon = 23.78303474471221 , init_zoom = 5;
 
-    var map = L.map('map').setView([37.97765252366751, 23.78303474471221], 16);
+    var map = L.map('map').setView([init_lon, init_lat], init_zoom);
 
     var url = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var mapboxurl = 'https://api.mapbox.com/styles/v1/aduitsis/cj0838v9n002k2sqpyabeut6q/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWR1aXRzaXMiLCJhIjoiY2owODJqdnFoMDRzaDMzcGVmeTNxNGJoMCJ9._b-ydVC6mADhaiwuEMjW4A';
 
-    L.tileLayer(url, {
+    L.tileLayer(mapboxurl, {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery � <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 23,
         //    id: 'your.mapbox.project.id',
@@ -45,21 +22,48 @@ define("main",['require','https://unpkg.com/leaflet@1.0.3/dist/leaflet.js'],func
     }).addTo(map);
 
 
-    L.geoJSON(f, {
-        onEachFeature: function (feature, layer) {
-        },
-        pointToLayer: function (feature, latlng) {
-            console.log(feature);
-            return L.marker(latlng, {
-                title: 'wifi access point',
-                alt: 'wifi access point',
-                opacity: 0.8,
-                //			icon: L.icon({
-                //				iconUrl: 'wifi.svg',
-                //				iconSize: 20,
-                //				iconAnchor:[10,20],
-                //			}),
-            });
-        },
-    }).addTo(map);
+	jQuery.ajax({
+		url: 'ntua.json',
+		dataType: 'json'}
+	).then(
+		function(data,testStatus,jqXHR) {
+			var min_lat=init_lat,min_lon=init_lon,max_lat=init_lat,max_lon=init_lon;
+			console.log( min_lat + ' - ' + max_lat );
+			console.log( min_lon + ' - ' + max_lon );
+			data.features.forEach( function(f) {
+				var lat = f.geometry.coordinates[1], lon = f.geometry.coordinates[0];
+				console.log('lat='+lat+' lon='+lon);
+				min_lon = ( lon < min_lon )? lon : min_lon;
+				max_lon = ( lon > max_lon )? lon : max_lon;
+				min_lat = ( lat < min_lat )? lat : min_lat;
+				max_lat = ( lat > max_lat )? lat : max_lat;
+			});
+			console.log( min_lon + ' - ' + max_lon );
+			console.log( min_lat + ' - ' + max_lat );
+			map.fitBounds(L.latLngBounds( L.latLng(min_lat,min_lon),L.latLng(max_lat,max_lon) ), { } );
+			L.geoJSON( data, {
+				onEachFeature: function (feature, layer) {
+				},
+				pointToLayer: function (feature, latlng) {
+					//console.log(feature);
+					return L.marker(latlng, {
+						title: 'wifi access point',
+						alt: 'wifi access point',
+						opacity: 0.8,
+						//			icon: L.icon({
+						//				iconUrl: 'wifi.svg',
+						//				iconSize: 20,
+						//				iconAnchor:[10,20],
+						//			}),
+					});
+				},
+			}).addTo(map);
+		},
+		function(data,textStatus,jqXHR) {
+			console.log('GET '+url+' failed: ' + textStatus);
+			console.log( 'Detailed error thrown follows:' );
+			console.log( errorThrown );
+		}
+	);
+
 });
--- map.js
+++ map.js
@@ -1,3 +1,8 @@
-requirejs(['main'],function(main) {
-
+require.config({
+	paths: {
+		jquery:'https://code.jquery.com',
+		leaflet:'https://unpkg.com/leaflet@1.0.3/dist',
+	},
+});
+require(['main'],function(main) {
 });
